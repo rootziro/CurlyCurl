7fffd31b79e301e001f6b37fc18d57c11a05fdca
rootziro <sploit55@gmail.com>
2025-02-08 11:38:59 +0000

    Request forms

--- curlycurl.py
+++ curlycurl.py
@@ -1,1 +1,6 @@
+import requests
 
+# Request Forms
+r = requests.get('http://google.com/calender')
+r = requests.head('http://google.com/get')
+r = requests.post('http://google.com/post',data={'key': 'value'})
