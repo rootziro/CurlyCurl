1aa715a2301d3b8ca945565a1e5d65393e4e37df
rootziro <sploit55@gmail.com>
2025-02-08 21:45:43 +0000

    Paremeter parser

--- curlycurl.py
+++ curlycurl.py
@@ -5,3 +5,10 @@ r = requests.get('http:// /')
 r = requests.head('http:// /get')
 r = requests.post('http:// /post',data={'key': 'value'})
 r = requests.options('http:// /get')
+
+# Passing parameters
+
+# Please do further research as to why the key value pairs was not feasble within this set(Pylance)
+payload = {'key1', 'value1', 'key2', 'value2'} # set value pairs are just examples
+r.requests.get('http:// /get', params=payload)
+print(r.url)
